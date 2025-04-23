189019f585e006b6752ffef18bcfd8bfbd17bbb3
jrstarke-WT <jrstarke@my.waketech.edu>
2025-04-23 14:56:31 -0400

    Update gradebook.js

--- public/gradebook.js
+++ public/gradebook.js
@@ -1,5 +1,22 @@
 function fetchGradeData() {
 	console.log("Fetching grade data...");
+
+	let xhr = new XMLHttpRequest();
+	let apiRoute = "/api/grades";
+
+	xhr.onreadystatechange = function(){
+		let results;
+
+		if(xhr.readystate === xhr.DONE){
+			if(xhr.status !== 200){
+				console.error('Could note get grades.
+			      Status: ${xhr.status}');
+			}
+			populateGradebook(JSON.parse(xhr.responseText));
+		}
+	}.bind(this);
+	xhr.open("get", apiRoute, true);
+	xhr.sent();
 }
 
 function populateGradeBook(data) {
